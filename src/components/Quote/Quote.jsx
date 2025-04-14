d42c8b18fd0c5b0d099769ad26104f50c537cfeb
kimviiii <kalanakivindu@gmail.com>
2025-04-14 20:53:15 +0530

    Refactor getQuote function to improve error handling and maintainability

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -5,26 +5,34 @@ function Quote() {
     const [quote, setQuote] = useState("Loading quote...");
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const getQuote = async () => {
-            const quoteURL = "/api/random";
-            try {
-                const response = await fetch(quoteURL);
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
+    const getQuote = async () => {
+        const quoteURL = "/api/random";
+        try {
+            const response = await fetch(quoteURL);
+            if (!response.ok) {
+                if(response.status === 429) {
+                    console.log("Rate limit exceeded. Please try again later.");
                 }
-                const data = await response.json();
-                const quoteText = `"${data[0].q}" - ${data[0].a}`;
-                setQuote(quoteText);
-                setError(null);
-            } catch (err) {
-                console.error("Error fetching quote:", err);
-                setQuote("No quote available at the moment.");
-                setError(err.message);
+                throw new Error("Network response was not ok");
             }
-        };
+            const data = await response.json();
+            const quoteText = `"${data[0].q}" - ${data[0].a}`;
+            setQuote(quoteText);
+            setError(null);
+        } catch (err) {
+            console.error("Error fetching quote:", err);
+            setQuote("No quote available at the moment.");
+            setError(err.message);
+        }
+    };
 
-        getQuote();
+    useEffect(() => {
+        getQuote(); // initial fetch
+        const intervalId = setInterval(() => {
+            getQuote(); // call getQuote every 10 seconds
+        }, 600000); // 10 minutes
+        return () => {clearInterval(intervalId); // cleanup on unmount
+        };
     }, []);
 
     return (
